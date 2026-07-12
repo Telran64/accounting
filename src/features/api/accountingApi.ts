65613fb5b033913c5876b508a4c4148c4eac044f
edward <edwardlfish@gmail.com>
2026-07-12 18:28:49 +0300

    Refactor updateUser API to use auth header from Redux state and simplify function parameters

--- src/features/api/accountingApi.ts
+++ src/features/api/accountingApi.ts
@@ -1,11 +1,22 @@
 import type {UserProfile, UserRegister, UserUpdate} from "../../utils/types";
 import {BASE_URL} from "../../utils/constants.ts";
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import type {RootState} from "../../app/store.ts";
+
+const authEndpoints = ['updateUser'];
 
 export const accountingApi = createApi({
     reducerPath: 'accountingApi',
     tagTypes: ['profile'],
-    baseQuery: fetchBaseQuery({baseUrl: BASE_URL}),
+    baseQuery: fetchBaseQuery({
+        baseUrl: BASE_URL,
+        prepareHeaders: (headers, {getState, endpoint}) => {
+            if (authEndpoints.includes(endpoint)) {
+                headers.set('Authorization', (getState() as RootState).token);
+            }
+            return headers;
+        }
+    }),
     endpoints: builder => ({
         registerUser: builder.mutation<UserProfile, UserRegister>({
             query: user => ({
@@ -24,14 +35,11 @@ export const accountingApi = createApi({
             }),
             providesTags: ['profile']
         }),
-        updateUser: builder.mutation<UserProfile, { user: UserUpdate, login: string, token: string }>({
-            query: ({user, login, token}) => ({
+        updateUser: builder.mutation<UserProfile, { user: UserUpdate, login: string }>({
+            query: ({user, login}) => ({
                 url: `/account/user/${login}`,
                 method: 'PATCH',
-                body: user,
-                headers: {
-                    Authorization: token
-                }
+                body: user
             }),
             invalidatesTags: ['profile']
         }),
@@ -48,4 +56,10 @@ export const accountingApi = createApi({
     })
 })
 
-export const {useRegisterUserMutation, useLazyFetchUserQuery, useFetchUserQuery, useChangePasswordMutation, useUpdateUserMutation} = accountingApi
+export const {
+    useRegisterUserMutation,
+    useLazyFetchUserQuery,
+    useFetchUserQuery,
+    useChangePasswordMutation,
+    useUpdateUserMutation
+} = accountingApi
